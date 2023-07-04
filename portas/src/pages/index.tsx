25892f30b7b88cb56c8945aae90f4203f605e220
term02 <ter02@gmail.com>
2023-07-03 21:05:49 -0300

    criando o componente jogo

--- portas/src/pages/index.tsx
+++ portas/src/pages/index.tsx
@@ -5,40 +5,6 @@ import { atualizarPortas, criarPortas } from '@/functions/portas'
 
 export default function Home() {
 
-  const [portas, setPortas] = useState(criarPortas(15,7,[5,2])) // eh uma funcao return new PortaModel(numeroDaPorta, temPresente)
-
-  function renderizarPortas(){
-    return portas.map(porta => {
-      return <Portas key={porta.qntPorta} value={porta} 
-        onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}/>
-      })
-  }
-
-  return (
-    <div style={{display:"flex", justifyContent:"center", flexWrap:"wrap"}}>
-
-        {renderizarPortas()}
   
-    </div>
-  )
 }
 
-//<Portas value={porta} onChange={novaPorta => console.log(novaPorta)} />
-
-/* vídeo: usando o evento onchange na PORTA ele funciona como um input next 07.mp4
-
-https://www.cod3r.com.br/courses/take/nextjs/lessons/25743627-adicionando-eventos-a-porta-02
-
-*/
-// const [porta, setPorta] = useState(new PortaModel(1))
-// // const [texto, setTexto] = useState('...')
-// return (
-//   <div style={{display:"flex"}}>
-//     {/* <input type="text" value={texto}
-//       onChange={e => setTexto(e.target.value)}/> */}
-//       <Portas value={porta} onChange={novaPorta => setPorta(novaPorta)}/>
-
-//   </div>
-// )
-
-//return <Portas key={porta.numero} value={porta} onChange={console.log}/>
---
+++ portas/src/pages/jogo.tsx
@@ -0,0 +1,43 @@
+import styles from '../styles/Jogo.module.css'
+import { useState } from "react"
+
+import Portas from "@/components/Portas"
+import { atualizarPortas, criarPortas } from "@/functions/portas"
+
+export default function jogo() {
+    const [portas, setPortas] = useState(criarPortas(20, 3, [5, 2])) // eh uma funcao return new PortaModel(numeroDaPorta, temPresente)
+
+    function renderizarPortas() {
+        return portas.map(porta => {
+            return <Portas key={porta.qntPorta} value={porta}
+                onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} />
+        })
+    }
+    return (
+        <div className={styles.jogo}>
+            <div className={styles.portas}>
+                {renderizarPortas()}
+            </div>
+        </div>
+    )
+}
+
+//<Portas value={porta} onChange={novaPorta => console.log(novaPorta)} />
+
+/* vídeo: usando o evento onchange na PORTA ele funciona como um input next 07.mp4
+
+https://www.cod3r.com.br/courses/take/nextjs/lessons/25743627-adicionando-eventos-a-porta-02
+
+*/
+// const [porta, setPorta] = useState(new PortaModel(1))
+// // const [texto, setTexto] = useState('...')
+// return (
+//   <div style={{display:"flex"}}>
+//     {/* <input type="text" value={texto}
+//       onChange={e => setTexto(e.target.value)}/> */}
+//       <Portas value={porta} onChange={novaPorta => setPorta(novaPorta)}/>
+
+//   </div>
+// )
+
+//return <Portas key={porta.numero} value={porta} onChange={console.log}/>
